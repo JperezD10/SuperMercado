938f7a295bcf14ac13866d8e3cd7b44d5df5b798
Julian Perez Demonty <jperezdemonty@live.com.ar>
2022-08-19 20:45:30 -0300

    Template de login y Register

---
+++ SuperMercado/Client/Models/Rutas.cs
@@ -0,0 +1,8 @@
+﻿namespace SuperMercado.Client.Models
+{
+    public struct Rutas
+    {
+        public const string Login = "/Login";
+        public const string Register = "/Register";
+    }
+}
