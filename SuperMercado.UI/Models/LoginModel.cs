60f6f1d8266e54c2e1dfcd6d469e69197c12fd80
Julian Perez Demonty <jperezdemonty@live.com.ar>
2022-09-02 12:13:10 -0300

    Login validation

---
+++ SuperMercado.UI/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperMercado.UI.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Username necesario")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password necesaria")]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,15}$", ErrorMessage = "Formato de contraseña incorrecta")]
+        public string Password { get; set; }
+    }
+}
